f21e7bba815ce756cd4641f1b285aca6256075e2
Bojan Jelaca <bojan.jelaca@zuehlke.com>
2017-06-14 10:41:04 +0200

    Initial commit

---
+++ Assets/Scripts/CylinderLogo.cs
@@ -0,0 +1,21 @@
+﻿using HoloToolkit.Unity.SpatialMapping;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CylinderLogo : TapToPlace
+    {
+	
+        // Update is called once per frame
+        protected override void Update ()
+        {
+            base.Update();
+
+            if (IsBeingPlaced)
+            {
+                gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x + 90,
+                    gameObject.transform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
+            }
+        }
+    }
+}
